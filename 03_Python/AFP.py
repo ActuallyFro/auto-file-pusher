8228e0dc0b7919936a119f462d6bf57490c61cc7
actuallyfro <actuallyfro@github.com>
2021-10-30 14:46:40 -0500

    {03_Python} Got File download from web + File-Content extraction working...

--- 03_Python/AFP.py
+++ 03_Python/AFP.py
@@ -2,6 +2,7 @@ import os #to allow for clearing the screen
 import socket #for downloading the file
 import datetime #to get clock time
 import time #To sleep
+import re #regex to parse downloaded text stream
 
 # Program Description:
 # Create the Auto-File-Puller
@@ -11,6 +12,8 @@ import time #To sleep
 # 3. Pull the file from the server IF IT EXISTS
 # 4. Creates a directory for the file
 # 5. Moves the downloaded file to the directory
+
+# Note: Requests exists -- I HATE libs on libs ... that ARE NOT STDLibs; python pushes you too... https://docs.python.org/3/library/http.client.html
 
 # -------------------------------------------------------------------
 #1 - Show the current time in HH:MM:SS format
@@ -43,30 +46,79 @@ def check_server_for_exists(HostIP, HostPort, file2downloadWithPath, debug=False
 
 # -------------------------------------------------------------------
 
+def GetResponseBytesFromHTTPStreamFile(fileName):
+	ResponseBytes=-1
+	ContentStr =""
+	StartString=b"Content-Length:"
+	# Open temp_response.txt in , read subbytes starting at StartString until end of line
+	with open(fileName, "rb") as f:
+		for line in f:
+			if line.startswith(StartString):
+				ContentStr = line.decode()
+				break
+
+	match = re.match(r'Content-Length: ([\d]+)', ContentStr)
+	if match:
+		ResponseBytes = int(match.group(1))
+
+	return ResponseBytes
+
+
 # -------------------------------------------------------------------
 # 3. Pull the file from the server IF IT EXISTS
-# def check_server(debug):
-# 	HostIP = "localhost"
-# 	HostPort = 8000
-# 	file2downloadWithPath = "test.txt"
-	
-# 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 	s.connect((HostIP, HostPort))
+def download_from_server(HostIP, HostPort, file2downloadWithPath, outputFile="superd00per.bin", debug=False):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HostIP, HostPort))
 
-# # Send request HEAD for file
+	s.send(b"GET /" + file2downloadWithPath.encode() + b" HTTP/1.1\r\n\r\n")
+
+	#A source: https://www.tomordonez.com/python-socket-save-data-to-file/
+	# with open('data_stream.txt', 'r') as fhandle:
+	# 		for line in fhandle:
+	# 				match = re.match(r'Last-Modified: ([\w,: ]+)', line)
+	# 				if match:
+	# 						last_modified = match.group()
+	# 						print(last_modified)
+	# 				match2 = re.match(r'ETag: (["\w-]+)', line)
+	# 						etag = match.group()
+	# 						print(etag)
+	#
+	# SO something like Content-type: (e.g., image/png) && Content-Length: (e.g., 52638) could be parsed
+
+	#save all only GET CONTENT to local file
+	with open("temp_response.txt", "wb") as f: #wb IF NOT decoding, else w!
+		while True:
+			data = s.recv(1024)
+			if not data:
+				break
+			f.write(data) #"utf-8" is default encoding
+			# f.write(data.decode())
 
 
-# 	s.send(b"GET /" + file2downloadWithPath.encode() + b" HTTP/1.1\r\n\r\n")
-# 	response = s.recv(1024)
-# 	s.close()
+	s.close()
 
-# 	if debug:
-# 		print(response)
+	ResponseBytes=GetResponseBytesFromHTTPStreamFile("temp_response.txt")
+	if debug:
+		print("Total Bytes from Content-Length: <"+str(ResponseBytes)+">")
 
-# 	if response.find(b"404") != -1:
-# 		return True
-# 	else:
-# 		return False
+	#Get total binary bytes from file, save in FileSize
+	FileSize = os.path.getsize("temp_response.txt")
+	SkipToBytes = FileSize-ResponseBytes
+
+	print("Skipping to Byte Position: "+str(SkipToBytes))	
+
+	# mmmm... large files?
+	ReadByteArray = open("temp_response.txt", "rb").read() #Read in all bytes
+	last_ResponseBytes_bytes = ReadByteArray[-ResponseBytes:] #Slice the LAST ResponseBytes
+	with open(outputFile, "wb") as f2:
+		f2.write(last_ResponseBytes_bytes)
+
+	#Delete temp_response.txt
+	os.remove("temp_response.txt")
+
+	return ResponseBytes
+
+
 # -------------------------------------------------------------------
 
 # -------------------------------------------------------------------
@@ -86,6 +138,7 @@ timeout= 0.5
 hasFileBeenFound = False
 strFileFoundAtTime =""
 hasFileBeenDownloaded = False
+strFileDownloadedAtTime = ""
 hasDirectoryBeenCreated = False
 hasFileBeenDownloadedAndPlacedInDirectory = False
 
@@ -111,7 +164,7 @@ while True:
 		print("Logs")
 		print("--------")
 		if hasFileDownloadedLogged:
-			print("File found...downloading...")
+			print("File found...downloading..."+strFileDownloadedAtTime)
 			# print("File downloaded: "+str(hasFileDownloadedLogged))
 
 	#Check server for file; when found log time
@@ -124,6 +177,15 @@ while True:
 	else: #File exists...
 		if not hasFileDownloadedLogged:
 			hasFileDownloadedLogged = True
+
+	if hasFileDownloadedLogged and not hasFileBeenDownloaded:
+		ResponseBytes = download_from_server("localhost", 8000, "file.png", "file.png")
+		curTimeStr = str(show_time()) #update time due to serial processing
+		strFileDownloadedAtTime = " [@"+curTimeStr+"] <Size: "+str(ResponseBytes)+">"
+
+		#check if file downloaded
+		if os.path.isfile("file.png"):
+			hasFileBeenDownloaded = True
 
 	# 	# # create directory
 	# 	# os.system("mkdir -p ./AFP_downloads/")
