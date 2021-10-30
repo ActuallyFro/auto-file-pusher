d2b8fd1cc15f810841816b7818a662a25cc0e942
actuallyfro <actuallyfro@github.com>
2021-10-30 12:16:13 -0500

    {03_Python} Adding Statuses, Logging, and finished file checks

--- 03_Python/AFP.py
+++ 03_Python/AFP.py
@@ -12,40 +12,47 @@ import time #To sleep
 # 4. Creates a directory for the file
 # 5. Moves the downloaded file to the directory
 
+# -------------------------------------------------------------------
+#1 - Show the current time in HH:MM:SS format
 def show_time():
-  # Show the current time in HH:MM:SS format
 	time = datetime.datetime.now()
 	return time.strftime("%H:%M:%S")
+# -------------------------------------------------------------------
 
+# -------------------------------------------------------------------
 #2 - connect to http://localhost:8000/ and check for a file every 500ms
 def check_server(debug):
 	HostIP = "localhost"
 	HostPort = 8000
 	file2downloadWithPath = "test.txt"
+	
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((HostIP, HostPort))
 
-	#send http get request for text.txt
 	s.send(b"GET /" + file2downloadWithPath.encode() + b" HTTP/1.1\r\n\r\n")
 	response = s.recv(1024)
 	s.close()
 
-	# Print the response
 	if debug:
 		print(response)
 
-	# evaluate the response for a 404 error
 	if response.find(b"404") != -1:
 		return True
 	else:
 		return False
+# -------------------------------------------------------------------
 
-	# if b"200 OK" in response:
-	# 	print("File found")
-	# 	return True
-	# else:
-	# 	print("File not found")
-	# 	return False
+# -------------------------------------------------------------------
+# 3. Pull the file from the server IF IT EXISTS
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# 4. Creates a directory for the file
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# 5. Moves the downloaded file to the directory
+# -------------------------------------------------------------------
 
 # main program
 # =============================================================================
@@ -54,32 +61,46 @@ toggleToShow = True
 timeout= 0.5
 
 hasFileBeenFound = False
+strFileFoundAtTime =""
 hasFileBeenDownloaded = False
 hasDirectoryBeenCreated = False
 hasFileBeenDownloadedAndPlacedInDirectory = False
 
+# Logs
+hasFileDownloadedLogged = False
+
 # loop: clear screen, show time, check server, pull file
 while True:
 	if toggleToShow:
+		curTimeStr = str(show_time())
 		os.system("clear")
 		print("Auto File Puller   <Ctrl+C to quit>")
 		print("================")
-		print("Current time: " + str(show_time()))
+		print("Current time: " + curTimeStr)
 		print("")
 		print("Statuses")
 		print("--------")
-		print("File found: "+str(hasFileBeenFound))
-		print("File downloaded: "+str(hasFileBeenFound))
+		print("File found: "+str(hasFileBeenFound)+strFileFoundAtTime)
+		print("File downloaded: "+str(hasFileBeenDownloaded))
 		print("File Placement Directory Created: "+str(hasDirectoryBeenCreated))
 		print("File placed in directory: "+str(hasFileBeenDownloadedAndPlacedInDirectory))
+		print("")
+		print("Logs")
+		print("--------")
+		if hasFileDownloadedLogged:
+			print("File found...downloading...")
+			# print("File downloaded: "+str(hasFileDownloadedLogged))
 
 
-	#Check server for file
+	#Check server for file; when found log time
 	if not hasFileBeenFound:
 		hasFileBeenFound = check_server(False)
+		if hasFileBeenFound:
+			strFileFoundAtTime = " [@"+curTimeStr+"]"
 	
 	else: #File exists...
-		print("File found...downloading...")
+		if not hasFileDownloadedLogged:
+			hasFileDownloadedLogged = True
 
 	# 	# # create directory
 	# 	# os.system("mkdir -p ./AFP_downloads/")
