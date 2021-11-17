31b479f880cb1c51db0986146c658b373a79f27b
actuallyfro <actuallyfro@github.com>
2021-11-16 18:59:47 -0600

    {04_PS} Test setup

--- 04_PS/AFP.ps1
+++ 04_PS/AFP.ps1
@@ -1,1 +1,9 @@
 #Going to try this one...
+
+#create variable logfile for file output name
+# $logfile = "logfile.txt";
+
+# Get-Date -Format 'yyyy-MM-dd HH:mm:ss' | Out-File -Encoding ASCII -FilePath $logfile
+$startTime=Get-Date -Format 'yyyy-MM-dd HH:mm:ss' 
+echo "Starting time: " $startTime
+
